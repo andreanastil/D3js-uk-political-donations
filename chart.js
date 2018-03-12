4a751d415a3fbd398823178508518addf7323cab
andreanastil <p15styl@ionio.gr>
2018-03-12 22:38:13 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -307,8 +307,8 @@ function moveToAmounts(alpha) {
 				centreX = svgCentre.x; // εάν το ποσό υπερβαίνει το maxVal πάλι θα μπει μαζί με τα μεγαλύτερα
 			}
 		
-		d.x += (centreX - d.x) * brake * alpha * 1.1;
-		d.y += (centreY - d.y) * (brake + 0.02) * alpha * 1.1;
+		d.x += (centreX - d.x) * (brake + 0.02) * alpha * 2;
+		d.y += (centreY - d.y) * (brake + 0.02) * alpha * 2;
 	};
 }
 
