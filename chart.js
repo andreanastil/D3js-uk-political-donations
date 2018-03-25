d7cd7ba82c526c4f1e4e46ace901189839f6ec4e
andreanastil <p15styl@ionio.gr>
2018-03-25 19:34:19 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -419,7 +419,7 @@ function mouseover(d, i) {
 	this.style.cursor="hand";
 	responsiveVoice.speak(donor + ' £' + amount);
 	//place image
-	if (imageFile){
+	if imageExists(imageFile){
 	var elem = document.createElement("img");
 	elem.src = imageFile;
 	elem.setAttribute("height", "42");
